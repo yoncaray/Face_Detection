67ee4f0e38abcac907cf7d6afb3fc2727b201df3
Yonca <rayyonca@gmail.com>
2023-05-15 19:09:07 +0300

    Face detection
    Haar cascade, Real-time detection

---
+++ Face_Detection_with_Haar_Cascade.py
@@ -0,0 +1,17 @@
+import cv2 
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("Face Detection", frame)
+    if cv2.waitKey(1) &0xFF == 13: break
+
+cap.release()
+cv2.destroyAllWindows()
